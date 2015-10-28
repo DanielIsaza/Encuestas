020bdbaa1c154fde4033e7f4437cdf095c93b95e
mafeosza <fercha0202@hotmail.com>
2015-10-27 20:23:33 -0500

    Vistas 1.1

---
+++ vistas/Index.php
@@ -0,0 +1,24 @@
+<!doctype>
+<html>
+	<head>
+		<title>Semestres</title>
+		
+
+		<!-- JQuery-->
+		<script src="//code.jquery.com/jquery-2.1.3.js"></script>
+
+		<script type="./main.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/main.css">
+	</head>
+	<body>
+		<header>
+			<div class="encabezado">
+				<h1>Programa CIDBA</h1>
+				<h2>Acta de concertaci&oacute;n acad&eacute;mica</h2>
+			</div>
+		</header>
+    	<div class="row">
+    		<h2>><a href="#">Semestre I</h2></a>
+    	</div>
+	</body>
+</html>
