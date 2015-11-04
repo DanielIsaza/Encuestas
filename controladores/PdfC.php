e5972744a3ee917b60af920a405d513c48db23ca
Maria Fernanda <fercha0202@hotmail.com>
2015-11-04 15:31:08 -0500

    generar pdf y graficos 1.0

--- controladores/PdfC.php
+++ controladores/PdfC.php
@@ -1,9 +1,38 @@
 <?php 
 
 include_once("../modelo/PDF.php");
+require_once ("../libs/jpgraph/src/jpgraph.php");
+require_once ("../libs/jpgraph/src/jpgraph_pie.php");
 
 if(isset($_POST['hola'])){
-  crear();
+
+	grafico();
+    crear();
+}
+
+function grafico()
+{ 
+	// Se define el array de valores y el array de la leyenda
+	$datos = array(2,2,1,1);
+	$leyenda = array("uno","dos","tres","cuatro");
+	 
+	//Se define el grafico
+	$grafico = new PieGraph(450,300);
+	
+	//Definimos el titulo 
+	$grafico->title->Set("Mi primer grafico de tarta");
+	$grafico->title->SetFont(FF_FONT1,FS_BOLD);
+	 
+	//Añadimos el titulo y la leyenda
+	$p1 = new PiePlot($datos);
+	$p1->SetLegends($leyenda);
+	$p1->SetCenter(0.4);
+	 
+	//Se muestra el grafico
+	$grafico->Add($p1);
+    $nombreImagen = '../img/temp/' . 'grafico' . '.png';
+    // Display the graph
+    $grafico->Stroke($nombreImagen);
 }
 
 function crear()
@@ -18,8 +47,10 @@ function crear()
 	  //  $pdf->Cell(0,10,'Printing line number '.$i,0,1);
 	//$pdf->Image('../img/pastel.png');
 	$pdf->Cell(0,10,'Mira mi oshita algo ashi seria el reporte de PDF con las graficas',0,1);
-	$pdf->Cell("", "", $pdf->Image('../img/pastel.png', $pdf->GetX(),$pdf->GetY()),'LR',0,'R');
-	$pdf->Output('reporte1.pdf','D');
-	//$pdf->Output();
+	//$pdf->Image($nombreImagen, $pdf->GetX() + 140, $pdf->GetY() - 25, 40, 30);
+	$pdf->Cell("", "", $pdf->Image('../img/temp/grafico.png', $pdf->GetX()+33,$pdf->GetY()),'LR',0,'R');
+	//$pdf->Output('reporte1.pdf','D');
+	$pdf->Output();
+	unlink('../img/temp/grafico.png');
 }
 ?>
