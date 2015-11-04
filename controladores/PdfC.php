d75ee77caddf2f41c562986305bd97007ec63150
Maria Fernanda <fercha0202@hotmail.com>
2015-11-04 12:54:05 -0500

    generar pdf 1.0.1

--- controladores/PdfC.php
+++ controladores/PdfC.php
@@ -14,8 +14,12 @@ function crear()
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
 	$pdf->SetFont('Times','',12);
-	for($i=1;$i<=40;$i++)
-	    $pdf->Cell(0,10,'Printing line number '.$i,0,1);
+	//for($i=1;$i<=10;$i++)
+	  //  $pdf->Cell(0,10,'Printing line number '.$i,0,1);
+	//$pdf->Image('../img/pastel.png');
+	$pdf->Cell(0,10,'Mira mi oshita algo ashi seria el reporte de PDF con las graficas',0,1);
+	$pdf->Cell("", "", $pdf->Image('../img/pastel.png', $pdf->GetX(),$pdf->GetY()),'LR',0,'R');
 	$pdf->Output('reporte1.pdf','D');
+	//$pdf->Output();
 }
 ?>
